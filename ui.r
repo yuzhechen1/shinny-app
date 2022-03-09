d38eed6ee48bcb5302ab6ce581414187c6904216
yuzhechen1 <100688873+yuzhechen1@users.noreply.github.com>
2022-03-09 16:39:49 +0100

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,10 +1,19 @@
 ui <- fluidPage(
+  theme = bs_theme(version = 5, bootswatch = "morph"),
   h1("Champions League Network Data Analytics"),
   p(style = "font-family:Impact",
     "Dashboard for Champions League from 1955 to 2015"),
-    navbarPage(title = "Home",
-             navbarMenu(title = "Descriptive Analytics",
-                        tabPanel(title = "Top teams and goals",
+    navbarPage(title = "CCL",
+               tabPanel("Welcome",
+                        h1(strong("Welcome to Champions of the Champions League (CCL)"), style = "font-size:50px;"),
+                        p("If you have wanted to know everything about the Champions League this application is perfect for you!
+             Here, you are able to discover not only which European football clubs and countries have perforemed the best, 
+             but also explore everything about the matches played and even predictions on who might play eachother in the furture, 
+             all over the entire 65+ years of champions league history. Now go out there and explore the past and potentional future of 
+             the Champions League!"), style = "font-size: 20px;"),
+               navbarMenu(title = "Descriptive Analytics",
+                        tabPanel(title = "Data retrievals",
+                                 dataTableOutput("raw_data"),
                                  sidebarLayout(
                                    sidebarPanel(
                                      selectInput(inputId = "season",
@@ -14,6 +23,7 @@ ui <- fluidPage(
                                    mainPanel(
                                      p("In this season, teams in the final round are as below:"),
                                      tableOutput("top_teams"),
+                                     tableOutput("Winner"),
                                    )
                                  ),# select a season to find the best performing team
                                  
@@ -22,6 +32,7 @@ ui <- fluidPage(
                                  tableOutput("summary_team"), #descriptive analysis group by team
                         ),
                         tabPanel(title = "League summary",
+                                 p("Know about the performance of countries that you're interested in:"),
                                  # sidebarLayout(
                                  #   sidebarPanel(
                                  #     selectInput(
@@ -58,18 +69,24 @@ ui <- fluidPage(
                                      # ),
                                    ),
                                    mainPanel(
+                                     p("How many trophies does each country win in all seasons?"),
                                      plotOutput("bar_chart_by_learegue")
                                    ),
                                  ),
                         ),
                         ),# Descriptive part
              navbarMenu(title = "Network Analysis",
-                        tabPanel(title = "Network descriptive analysis",
-                                 p("This is the top 30 teams ordered by degree:"),
-                                 tableOutput("degree_table"),
-                                 p("This is the degree histogram of all teams:"),
+                        tabPanel(title = "Team data network",
+                                 p("Summary of degrees of the team network"),
+                                 dataTableOutput("degree_table"),
+                                 p("Degree histogram of all teams:"),
                                  plotOutput("Degree_distribution"),
-                                 p("This is the degree histogram of all countries:"),
+                                 
+                                 ),
+                        tabPanel(title = "Country data network",
+                                 p("Summary of degrees of the country network"),
+                                 dataTableOutput("degree_table.c"),
+                                 p("Degree histogram of all countries:"),
                                  plotOutput("Degree_distribution_c")
                                  ),
                         tabPanel(title = "Projection into the Teams Space",
@@ -86,7 +103,22 @@ ui <- fluidPage(
                                      plotOutput("projection")
                                    )
                                  ),
-                                 )
+                                 ),
+                        tabPanel(title = "Projection into the Countries Space",
+                                 sidebarLayout(
+                                   sidebarPanel(
+                                     selectInput(
+                                       inputId = "Seasons.c",
+                                       label = "Choose seasons for projection:",
+                                       choices = all_seasons$Season,
+                                       multiple = TRUE
+                                     )
+                                   ),
+                                   mainPanel(
+                                     plotOutput("projection.c")
+                                   )
+                                 ),
+                        ),
                         ),# Network Attributes part
              navbarMenu(title = "Deep Dive Analytics",
                         tabPanel(title = "Link prediction",
@@ -94,8 +126,10 @@ ui <- fluidPage(
                                  plotOutput("predicted_link")
                                  ),
                         tabPanel(title = "Inference",
+                                 plotOutput("Regression_results"),
+                                 tableOutput("significance")
                                  ## need to input variable names(such as centrality and average goals)
-                                 plotOutput("Regression_results")
+                                 
                           
                         )
                         ),# Deep dive analytics part
